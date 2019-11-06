ed1d8f6da617e7f69ccfb4651ac474d73fece9d2
Jeremy Fisher <jeremy@adamsfisher.me>
2019-11-06 17:26:57 -0500

    progress

--- Makefile
+++ Makefile
@@ -2,22 +2,27 @@
 
 PY=pipenv run python
 
-DATADIR=$(PWD)
-RAW_IMAGES_DIR=./outdir/raw_iamges
-CONVERTED_PNG_IMAGES_DIR=./outdir/converted_images
-LATENT_ENCODINGS_FP=./outdir/image_encodings
+RAW_IMAGES_DIR=/Users/jeremyfisher/Downloads/TCGA_DATA/
+CONVERTED_PNG_IMAGES_DIR=./outdir/converted_images/
+SPLIT_DATA_DIR=./outdir/split_data/
+LATENT_ENCODINGS_FP=./outdir/image_encodings.csv
 IMAGE_METADATA=./data/histology_image_annotations.csv
 
-default: convert autoencoder survival
+default: split
 
 convert:
 	$(PY) ./scripts/preprocessing/svs2png.py \
 		-s $(RAW_IMAGES_DIR) \
 		-o $(CONVERTED_PNG_IMAGES_DIR)
 
+split:
+	$(PY) ./scripts/preprocessing/split_data.py \
+		-i $(CONVERTED_PNG_IMAGES_DIR) \
+		-o $(SPLIT_DATA_DIR) \
+		'0.5,0.3,0.2'
+
 autoencoder:
 	$(PY) ./scripts/analysis/autoencoder.py \
-		'0.5,0.3,0.2' \
 		-i $(CONVERTED_PNG_IMAGES_DIR) \
 		-o $(LATENT_ENCODINGS_FP)
 
--- scripts/preprocessing/svs2png.py
+++ scripts/preprocessing/svs2png.py
@@ -1,20 +1,19 @@
 import glob
 import math
 import multiprocessing as mp
-import numpy as np
 import os
-import PIL
-from PIL import Image
 import argparse
 
-try:
-    import openslide
-except ImportError:
-    raise ImportError("Please run 'pip install openslide-python'")
+import openslide
+import PIL
+import numpy as np
+
+from pathlib import Path
+from PIL import Image
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--source-dir", dest="source_dir", default="")
-parser.add_argument("-o", "--output-dir", dest="outdir", default="")
+parser.add_argument("-s", "--source-dir", dest="source_dir", default="./")
+parser.add_argument("-o", "--output-dir", dest="outdir", default="./")
 parser.add_argument("--scale_factor", default=32, type=int)
 opt = parser.parse_args()
 
@@ -23,16 +22,11 @@ out_dir = opt.outdir
 scale_factor = opt.scale_factor
 
 
-def svs2png(pool):
+def svs2png(slide_path):
+    """Convert a WSI training slide to a saved scaled-down image in a format such as jpg or png.
+    Args:
+      slide_number: The slide number.
     """
-  Convert a WSI training slide to a saved scaled-down image in a format such as jpg or png.
-  Args:
-    slide_number: The slide number.
-  """
-
-    slide_path = pool.get()
-    if slide_path is None:
-        return
     img = svs2scaled_PIL(slide_path)
 
     img_outpath = out_dir + slide_path.split('/')[-1] + '.png'
@@ -42,13 +36,12 @@ def svs2png(pool):
 
 
 def svs2scaled_PIL(slide_path):
+    """Convert a WSI training slide to a scaled-down PIL image.
+    Args:
+      slide_path: The slide number.
+    Returns:
+      Tuple consisting of scaled-down PIL image, original width, original height, new width, and new height.
     """
-  Convert a WSI training slide to a scaled-down PIL image.
-  Args:
-    slide_path: The slide number.
-  Returns:
-    Tuple consisting of scaled-down PIL image, original width, original height, new width, and new height.
-  """
 
     slide = openslide.open_slide(slide_path)
 
@@ -61,26 +54,5 @@ def svs2scaled_PIL(slide_path):
     img = whole_image.resize((ds_width, ds_height), PIL.Image.BILINEAR)
     return img
 
-
-def mp_convert(file_paths):
-    """
-  Convert all WSI training slides to smaller images using multiple processes (one process per core).
-  Each process will process a range of slide numbers.
-  """
-
-    pool = mp.Pool(len(file_paths))
-
-    workers = [mp.Process(svs2png, args=pool) for i in range(mp.cpu_count())]
-    for worker in workers:
-        worker.start()
-
-    for worker in workers:
-        worker.join()
-
-
-file_paths = []
-for root, dirs, files in os.walk(os.path.abspath(source_dir)):
-    for file in files:
-        file_paths.append(os.path.join(root, file))
-
-mp_convert(file_paths)
+for fp in Path(source_dir).glob("*.svs"):
+    svs2png(str(fp))
